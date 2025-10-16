ae882443ea3b2b98d00e5a7d22f86a1760185989
9beach <9beach@gmail.com>
2025-10-16 13:17:54 +0900

    Add test cases

--- tests/test-epub-merge.sh
+++ tests/test-epub-merge.sh
@@ -14,7 +14,7 @@ cleanup() {
 trap cleanup EXIT
 
 epub_diff() {
-	echo "│   ↳ $1 ── $2"
+	echo "│   ↳ ""$(echo "$1" | sed "s#$epub_merge_dir/tests/##") ── $2"
 	"$epub_merge_dir/tests/epub-diff.sh" "$@"
 }
 
@@ -23,14 +23,16 @@ epub_merge() {
 }
 
 tcd() {
+	rm -rf "$temp_dir/$1"
 	mkdir -p "$temp_dir/$1"
 	cd "$temp_dir/$1"
 }
 
 temp_dir="$(mktemp -d)"
+cd "$temp_dir"
 
 for dir in "samples" "samples-v3"; do
-	SAMPLE_DIR="$epub_merge_dir/tests/$dir"
+	sample_dir="$epub_merge_dir/tests/$dir"
 
 	echo ".   epub-merge test: $dir"
 
@@ -38,17 +40,15 @@ for dir in "samples" "samples-v3"; do
 
 	echo ├── epub-merge test: setup
 
-	rsync -a --delete "$SAMPLE_DIR/" "$temp_dir"
-
 	########
 
 	tcd .merged
 	echo ├── epub-merge test: merge original
 
 	# shellcheck disable=SC2012
-	epub_merge -q ../original/*.epub
+	epub_merge -q "$sample_dir"/original/*.epub
 
-	epub_diff "sample.epub" ../merged/sample.epub
+	epub_diff "$sample_dir"/merged/sample.epub "sample.epub" 
 
 	########
 
@@ -56,13 +56,13 @@ for dir in "samples" "samples-v3"; do
 	echo ├── epub-merge test: merge splitted
 
 	# shellcheck disable=SC2012
-	epub_merge -q ../splitted/*.epub
+	epub_merge -q "$sample_dir"/splitted/*.epub
 
 	mv sample.epub 2nd-merge.epub
 	epub_merge -xfq 2nd-merge.epub
 
 	for i in sample?.epub; do
-		epub_diff "$i" ../splitted/"$i"
+		epub_diff "$sample_dir"/splitted/"$i" "$i" 
 	done
 
 	epub_merge -q sample?.epub
@@ -74,7 +74,7 @@ for dir in "samples" "samples-v3"; do
 	epub_merge -xfq 3rd-merge.epub
 
 	for i in sample?.epub; do
-		epub_diff "$i" ../splitted/"$i"
+		epub_diff "$sample_dir"/splitted/"$i" "$i" 
 	done
 
 	########
@@ -82,11 +82,10 @@ for dir in "samples" "samples-v3"; do
 	tcd .splitted
 	echo ├── epub-merge test: -x option
 
-	epub_merge -q -x ../merged/sample.epub
+	epub_merge -q -x "$sample_dir"/merged/sample.epub
 
-	cd ../splitted
-	for i in *.epub; do
-		epub_diff ../.splitted/"$i" "$i"
+	for i in sample?.epub; do
+		epub_diff "$sample_dir/splitted/$i" "$i"
 	done
 
 	########
@@ -94,16 +93,16 @@ for dir in "samples" "samples-v3"; do
 	tcd .merged-O
 	echo ├── epub-merge test: -O option
 
-	epub_merge -qO ../original/sample1.epub ../original/sample3.epub \
-		../original/sample2.epub
+	epub_merge -qO "$sample_dir"/original/sample1.epub \
+		"$sample_dir"/original/sample3.epub \
+		"$sample_dir"/original/sample2.epub
 
-	epub_diff sample.epub ../merged-O/sample.epub
+	epub_diff "$sample_dir"/merged-O/sample.epub sample.epub 
 
 	epub_merge -qx sample.epub
 
-	cd ../splitted
-	for i in *.epub; do
-		epub_diff ../.merged-O/"$i" "$i" 
+	for i in sample?.epub; do
+		epub_diff "$sample_dir/splitted/$i" "$i" 
 	done
 
 	########
@@ -111,15 +110,14 @@ for dir in "samples" "samples-v3"; do
 	tcd .merged-lsp
 	echo ├── epub-merge test: -l, -s, -p options
 
-	epub_merge -ql ko -s "번째 책" ../original/*.epub
+	epub_merge -ql ko -s "번째 책" "$sample_dir"/original/*.epub
 
-	epub_diff sample.epub ../merged-lsp/sample.epub
+	epub_diff "$sample_dir"/merged-lsp/sample.epub sample.epub
 
 	epub_merge -qx sample.epub
 
-	cd ../splitted
-	for i in *.epub; do
-		epub_diff ../.merged-lsp/"$i" "$i" 
+	for i in sample?.epub; do
+		epub_diff "$sample_dir/splitted/$i" "$i" 
 	done
 
 	########
@@ -127,13 +125,14 @@ for dir in "samples" "samples-v3"; do
 	tcd .force-write
 	echo "├── epub-merge test: -f option"
 
-	epub_merge -q ../original/*.epub
+	epub_merge -q "$sample_dir"/original/*.epub
 	cp sample.epub copied
-	epub_merge -q ../original/*.epub 2> /dev/null && false || true
+	epub_merge -q "$sample_dir"/original/*.epub \
+		2> /dev/null && false || true
 
-	# If merged again, UUID changed, then epub_diff returns 0, diff returns 1
+	# If merged again, UUID changed, then epub_diff -> 0, diff -> 1
 	diff -q sample.epub copied
-	epub_merge -qf ../original/*.epub
+	epub_merge -qf "$sample_dir"/original/*.epub
 	diff -q sample.epub copied > /dev/null 2>&1 && false || true
 	epub_diff sample.epub copied
 
@@ -143,24 +142,24 @@ for dir in "samples" "samples-v3"; do
 	echo "├── epub-merge test: -d option"
 
 	cd ..
-	epub_merge -qd .target-dir original/*.epub
+	epub_merge -qd .target-dir "$sample_dir/original"/*.epub
 
-	epub_diff .target-dir/sample.epub merged/sample.epub 
+	epub_diff "$sample_dir/merged/sample.epub" .target-dir/sample.epub 
 
 	########
 
 	tcd .title
 	echo "├── epub-merge test: -t, -v option"
 
-	epub_merge -q -v "haha//hoho//heehee" -t "&&<>:/sample" ../original/*.epub
-	epub_diff "&&____sample.epub" "../merged-tv/sample.epub"
+	epub_merge -q -v "haha//hoho//heehee" -t "&&<>:/sample" "$sample_dir"/original/*.epub
+	epub_diff "$sample_dir"/merged-tv/sample.epub "&&____sample.epub" 
 
 	########
 
 	tcd .name
 	echo "├── epub-merge test: -n option"
 
-	epub_merge -q -n "test hahaha" ../original/*.epub
+	epub_merge -q -n "test hahaha" "$sample_dir"/original/*.epub
 	[[ -f "test hahaha.epub" ]]
 
 	########
@@ -169,11 +168,27 @@ for dir in "samples" "samples-v3"; do
 	echo "├── epub-merge test: -d option"
 
 	cd ..
-	epub_merge -qd '.dir' original/*.epub
+	epub_merge -qd '.dir' "$sample_dir/original"/*.epub
 
-	epub_diff .dir/sample.epub merged/sample.epub 
+	epub_diff "$sample_dir/merged/sample.epub" .dir/sample.epub 
 
 	########
 
 	echo "└── all done"
 done
+
+sample_dir="$epub_merge_dir/tests/samples"
+
+times=10
+
+time ( for i in $(seq 1 $times); do epub_merge -f "$sample_dir"/original/* \
+	2> /dev/null; done )
+
+echo
+echo "$times merge completed"
+
+time ( for i in $(seq 1 $times); do epub_merge \
+	-fx "$sample_dir"/merged/sample.epub 2> /dev/null; done )
+
+echo
+echo "$times extract completed"
